e2f85b9c7a7c1db29aee5596f0c137854b8fc731
Timmeahj <broostim@hotmail.be>
2024-11-23 11:22:02 +0100

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
 import Image from "next/image";
 import Header from '../../components/organisms/Header';
 import Footer from '../../components/organisms/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Hero from "../../components/organisms/Hero";
 import Main from "../../components/organisms/Main";
+import favicon from '../../public/favicon.png';
+
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = favicon.src;
+    document.head.appendChild(link);
+  }, []);
+
   return (
     <div>
       <Header />
